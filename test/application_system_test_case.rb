aef0a1fa0f9b51243832f56b7ac361f81c6155a9
debjohnson33 <debjohnson_11@yahoo.com>
2019-06-13 16:37:48 -0500

    Changed typo/fixed bug

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,4 @@
 require "test_helper"
-require 'capybara/poltergeist'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :poltergeist
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ SimpleCov.start
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'capybara/poltergeist'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
