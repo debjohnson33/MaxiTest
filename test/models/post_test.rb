1f4f823084457a5b60661401801c148eae2187b1
debjohnson33 <debjohnson_11@yahoo.com>
2019-06-13 16:13:46 -0500

    Added post model tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = Post.new 
+  end
+  
+  test "post should not be valid without a body" do
+    refute @post.valid?
+  end
+
+  test "post should be valid with a body" do
+    @post.body = 'test'
+    assert @post.valid?
+  end
 end
